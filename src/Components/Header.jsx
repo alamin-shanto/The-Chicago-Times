c252c4c7eef5bfd8cb0f2f79d5baeb182292fb46
alamin-shanto <mdalaminshanto1993@gmail.com>
2025-05-04 04:09:00 +0600

    added time interval

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,17 @@
 import { format } from "date-fns";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Header = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="mt-10 text-center space-y-3">
       <h1 className="jacquard-24 font-bold text-6xl text-[#444444]">
@@ -10,6 +20,9 @@ const Header = () => {
       <p className="text-accent">Journalism Without Fear or Favour</p>
       <p className="text-xl text-accent font-semibold">
         {format(new Date(), "EEEE, MMMM MM, yyyy")}
+      </p>
+      <p className="text-xl text-accent font-semibold">
+        {format(currentTime, "hh : mm : ss aa")}
       </p>
     </div>
   );
